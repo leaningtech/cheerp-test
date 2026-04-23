efb933edf175bd3457dc320b5df51d8b7307892e
Yuri Iozzelli <y.iozzelli@gmail.com>
2026-04-23 15:40:26 +0200

    fix jsexport/factory: add reserved name for manual js variable

--- tests/jsexport/factory.cpp
+++ tests/jsexport/factory.cpp
@@ -6,26 +6,26 @@
 // REQUIRES: regular
 
 // Test vanilla module
-// RUN: regular_only run_if_js compile_mode_js -o %t/vanilla.js %s 2>&1
+// RUN: regular_only run_if_js compile_mode_js -cheerp-reserved-names="X" -o %t/vanilla.js %s 2>&1
 // RUN: regular_only run_if_js %node %t/vanilla.js 2>&1 | %FileCheck %s
 
 // Test ES6 module
-// RUN: regular_only run_if_js compile_mode_js -cheerp-make-module=es6 -o %t/es6.mjs %s 2>&1
+// RUN: regular_only run_if_js compile_mode_js -cheerp-reserved-names="X" -cheerp-make-module=es6 -o %t/es6.mjs %s 2>&1
 // RUN: regular_only run_if_js python3 %helpers/run_cheerp_module.py --module=es6 %t/es6.mjs 2>&1 | %FileCheck %s
 
 // Test CommonJS module
-// RUN: regular_only run_if_js compile_mode_js -cheerp-make-module=commonjs -o %t/commonjs.js %s 2>&1
+// RUN: regular_only run_if_js compile_mode_js -cheerp-reserved-names="X" -cheerp-make-module=commonjs -o %t/commonjs.js %s 2>&1
 // RUN: regular_only run_if_js python3 %helpers/run_cheerp_module.py --module=commonjs %t/commonjs.js 2>&1 | %FileCheck %s
 
 // Test Closure module
-// RUN: regular_only run_if_js compile_mode_js -cheerp-make-module=closure -o %t/closure.js %s 2>&1
+// RUN: regular_only run_if_js compile_mode_js -cheerp-reserved-names="X" -cheerp-make-module=closure -o %t/closure.js %s 2>&1
 // RUN: regular_only run_if_js %node %t/closure.js 2>&1 | %FileCheck %s
 
 // Also test wasm and asmjs with vanilla driver:
-// RUN: regular_only run_if_wasm compile_mode_wasm -o %t/w %s 2>&1
+// RUN: regular_only run_if_wasm compile_mode_wasm -cheerp-reserved-names="X" -o %t/w %s 2>&1
 // RUN: regular_only run_if_wasm %node %t/w 2>&1 | %FileCheck %s
 
-// RUN: regular_only run_if_asmjs compile_mode_asmjs -o %t/a %s 2>&1
+// RUN: regular_only run_if_asmjs compile_mode_asmjs -cheerp-reserved-names="X" -o %t/a %s 2>&1
 // RUN: regular_only run_if_asmjs %node %t/a 2>&1 | %FileCheck %s
 
 
