49f65a2ca34096e613cea53dff291552a7ab36fa
Yuri Iozzelli <y.iozzelli@gmail.com>
2026-04-24 16:02:20 +0200

    split std/test4.cpp in two

---
+++ tests/std/stringstream.cpp
@@ -0,0 +1,28 @@
+//===---------------------------------------------------------------------===//
+//	Copyright 2025 Leaning Technologies
+//===---------------------------------------------------------------------===//
+
+// RUN: mkdir -p %t
+// RUN: %regular_only %run_if_js %compile_mode_js -o %t/j %s 2>&1 && node %t/j 2>&1 | %FileCheck %s
+// RUN: %regular_only %run_if_wasm %compile_mode_wasm -o %t/w %s 2>&1 && node %t/w 2>&1 | %FileCheck %s
+// RUN: %regular_only %run_if_asmjs %compile_mode_asmjs -o %t/a %s 2>&1 && node %t/a 2>&1 | %FileCheck %s
+// RUN: %preexec_only %run_if_js %compile_mode_js -o %t/j %s 2>&1 | %FileCheck %s
+// RUN: %preexec_only %run_if_asmjs %compile_mode_asmjs -o %t/a %s 2>&1 | %FileCheck %s
+
+#include <tests.h>
+#include <sstream>
+
+void webMain()
+{
+	std::ostringstream str;
+	str << "test " << 42;
+	assertPrint("ostringstream write:", str.str().c_str());
+	// CHECK: ostringstream write: test 42
+
+	std::stringstream str2;
+	str2 << "43";
+	int res;
+	str2 >> res;
+	assertPrint("stringstream read int:", res);
+	// CHECK: stringstream read int: 43
+}
